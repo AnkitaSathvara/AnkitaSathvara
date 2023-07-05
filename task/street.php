65023b9d66d9a7a73b0c6ca5010612459f4cfba6
AnkitaSathvara <137819918+AnkitaSathvara@users.noreply.github.com>
2023-07-05 09:56:30 +0100

    Update street.php

--- task/street.php
+++ task/street.php
@@ -1,1 +1,19 @@
 
+<?php
+
+// Simulate API call to retrieve street information
+$street = $_POST['street'];
+
+// Perform any necessary processing or API calls
+// Replace this with your actual logic to retrieve street information from an API
+
+// Simulated response
+$response = [
+    'street' => $street,
+    'length' => '5 km',
+    'width' => '10 m',
+];
+
+header('Content-Type: application/json');
+echo json_encode($response);
+?>

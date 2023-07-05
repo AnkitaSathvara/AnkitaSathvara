f25e5566f434e4140bdd465a637c78151cefe4e0
AnkitaSathvara <137819918+AnkitaSathvara@users.noreply.github.com>
2023-07-05 09:55:38 +0100

    Update address.php

--- task/address.php
+++ task/address.php
@@ -1,1 +1,20 @@
+<?php
+
+// Simulate API call to retrieve address information
+$address = $_POST['address'];
+
+// Perform any necessary processing or API calls
+// Replace this with your actual logic to retrieve address information from an API
+
+// Simulated response
+$response = [
+    'address' => $address,
+    'city' => 'City Name',
+    'state' => 'State Name',
+    'country' => 'Country Name',
+];
+
+header('Content-Type: application/json');
+echo json_encode($response);
+?>
 

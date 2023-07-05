7b60e23a3bdc50d9c3119e7ee735c8c03e2ff1c6
AnkitaSathvara <137819918+AnkitaSathvara@users.noreply.github.com>
2023-07-05 09:56:04 +0100

    Update children.php

--- task/children.php
+++ task/children.php
@@ -1,1 +1,22 @@
 
+<?php
+
+// Simulate API call to retrieve children information
+$parent = $_POST['parent'];
+
+// Perform any necessary processing or API calls
+// Replace this with your actual logic to retrieve children information from an API
+
+// Simulated response
+$response = [
+    'parent' => $parent,
+    'children' => [
+        'Child 1',
+        'Child 2',
+        'Child 3',
+    ],
+];
+
+header('Content-Type: application/json');
+echo json_encode($response);
+?>

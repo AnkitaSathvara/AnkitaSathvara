6f0d3398c7011c86cd39e1d4a309aa8840a8a6b3
AnkitaSathvara <137819918+AnkitaSathvara@users.noreply.github.com>
2023-07-07 12:52:02 +0100

    Update index.php

--- task/index.php
+++ task/index.php
@@ -1,1 +1,70 @@
+<!doctype html>
 
+<html lang="en">
+
+	<head>
+
+		<meta charset="utf-8">
+
+		<title>AJAX/PHP/CURL/JSON example</title>
+		<meta name="description" content="AJAX/PHP/CURL/JSON example">
+		<meta name="author" content="Ankitaben Sathvara">
+
+	</head>
+
+	<body>
+
+		<select id="selPostalcode">
+			<option value="8775">8775</option>
+			<option value="8774">8774</option>
+			<option value="8772">8772</option>
+			<option value="8784">8784</option>
+		</select>
+
+		<select id="selCountrycode">
+			
+			<option value="CH">CH</option>
+		</select>
+
+
+		<button id="btnRun">Run</button>
+
+		<br><br>
+
+		<div id="divResults">
+			
+			<table>
+
+				<tr>
+					<td align="right">
+						Langitude: 
+					</td>
+					<td id="txtLangitude">
+						
+					</td>
+
+				</tr>
+
+				<tr>
+					<td align="right">
+						Latitude: 
+					</td>
+					<td id="txtLatitude">
+						
+					</td>
+
+				</tr>
+
+				
+
+				
+			</table>
+
+		</div>
+
+		<script type="application/javascript" src="libs/js/jquery-2.2.3.min.js"></script>
+		<script type="application/javascript" src="libs/js/script.js"></script>
+
+	</body>
+
+</html>

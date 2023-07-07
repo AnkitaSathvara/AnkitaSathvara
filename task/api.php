fc9bc103da307e137cc5cb6f73c9b364d8b0d1e9
AnkitaSathvara <137819918+AnkitaSathvara@users.noreply.github.com>
2023-07-07 12:42:19 +0100

    Update api.php

--- task/api.php
+++ task/api.php
@@ -1,22 +1,2 @@
-<?php
-// Handle the request and return JSON response
-$username = $_POST['username'];
-$latitude = $_POST['latitude'];
-$longitude = $_POST['longitude'];
 
-// Perform necessary operations based on the API endpoint
-
-// Example response
-$response = [
-  'status' => 'success',
-  'message' => 'API call successful',
-  'data' => [
-    'latitude' => $latitude,
-    'longitude' => $longitude
-  ]
-];
-
-header('Content-Type: application/json');
-echo json_encode($response);
-?>
 

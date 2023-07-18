7a0b05b32c2d1f6b1709f2e9e1a454cc5804c786
AnkitaSathvara <137819918+AnkitaSathvara@users.noreply.github.com>
2023-07-18 22:21:59 +0100

    Create script2.js

---
+++ task/script2.js
@@ -0,0 +1,28 @@
+	$('#btnRun').click(function() {
+
+		$.ajax({
+			url: "libs/php/langitudeandlatitude.php",
+			type: 'POST',
+			dataType: 'json',
+			data: {
+				country: $('#selCountry1').val(),
+				lang: $('#selLanguage1').val()
+			},
+			success: function(result) {
+
+				console.log(JSON.stringify(result));
+
+				if (result.status.name == "ok") {
+
+					$('#txtLat').html(result['data'][0]['lat']);
+					$('#txtLong').html(result['data'][0]['lng']);
+					
+				}
+			
+			},
+			error: function(jqXHR, textStatus, errorThrown) {
+				// your error code
+			}
+		}); 
+	
+	});

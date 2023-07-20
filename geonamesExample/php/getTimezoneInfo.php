a47bd01e01ae9f32a58986ee346dc1eed79457fd
AnkitaSathvara <137819918+AnkitaSathvara@users.noreply.github.com>
2023-07-20 14:01:35 +0100

    Create getTimezoneInfo.php

---
+++ geonamesExample/php/getTimezoneInfo.php
@@ -0,0 +1,36 @@
+<?php
+
+	// remove for production
+
+	ini_set('display_errors', 'On');
+	error_reporting(E_ALL);
+
+	$executionStartTime = microtime(true);
+
+	$url='http://api.geonames.org/timezoneJSON?lat='. $_REQUEST['lat'].'&lng='. $_REQUEST['lng'].'&username=flightltd';
+
+	//
+	 // $url='http://api.geonames.org/timezoneJSON?lat=22.20775&lng=76.97021&username=flightltd';
+
+	$ch = curl_init();
+	curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+	curl_setopt($ch, CURLOPT_URL,$url);
+
+	$result=curl_exec($ch);
+
+	curl_close($ch);
+
+	 $decode = json_decode($result,true);	
+    //print_r($decode);exit;
+	$output['status']['code'] = "200";
+	$output['status']['name'] = "ok";
+	$output['status']['description'] = "success";
+	$output['status']['returnedIn'] = intval((microtime(true) - $executionStartTime) * 1000) . " ms";
+	$output['data'] = $decode;
+	
+	header('Content-Type: application/json; charset=UTF-8');
+
+	echo json_encode($output); 
+
+?>

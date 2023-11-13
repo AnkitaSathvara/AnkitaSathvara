f6303406e4fc23324fac1837eb0ec76868808e29
AnkitaSathvara <137819918+AnkitaSathvara@users.noreply.github.com>
2023-11-13 09:40:39 +0000

    Add files via upload

--- project2/libs/sql/companydirectory.sql
+++ project2/libs/sql/companydirectory.sql
@@ -2,20 +2,17 @@
 -- Host:                         127.0.0.1
 -- Server version:               10.4.6-MariaDB - mariadb.org binary distribution
 -- Server OS:                    Win64
--- HeidiSQL Version:             11.1.0.6116
+-- HeidiSQL Version:             11.3.0.6333
 -- --------------------------------------------------------
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET NAMES utf8 */;
 /*!50503 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-
-
--- Dumping database structure for companydirectory
-CREATE DATABASE IF NOT EXISTS `dbs1508494` /*!40100 DEFAULT CHARACTER SET utf8 */;
-USE `dbs1508494`;
 
 -- Dumping structure for table companydirectory.department
 CREATE TABLE IF NOT EXISTS `department` (
@@ -23,9 +20,9 @@ CREATE TABLE IF NOT EXISTS `department` (
   `name` varchar(50) DEFAULT NULL,
   `locationID` int(11) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=15 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=20 DEFAULT CHARSET=utf8;
 
--- Dumping data for table companydirectory.department: ~13 rows (approximately)
+-- Dumping data for table companydirectory.department: ~12 rows (approximately)
 /*!40000 ALTER TABLE `department` DISABLE KEYS */;
 INSERT INTO `department` (`id`, `name`, `locationID`) VALUES
 	(1, 'Human Resources', 1),
@@ -175,7 +172,8 @@ INSERT INTO `personnel` (`id`, `firstName`, `lastName`, `jobTitle`, `email`, `de
 	(100, 'Shae', 'Johncey', '', 'sjohncey2r@bluehost.com', 10);
 /*!40000 ALTER TABLE `personnel` ENABLE KEYS */;
 
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
-/*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
+/*!40014 SET FOREIGN_KEY_CHECKS=IFNULL(@OLD_FOREIGN_KEY_CHECKS, 1) */;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+/*!40111 SET SQL_NOTES=IFNULL(@OLD_SQL_NOTES, 1) */;
